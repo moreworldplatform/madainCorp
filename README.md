ca5050f43b048f1be470ad84e40c2179addd9f7f
Mohamed El Azazy <moregroupx@yahoo.com>
2021-06-21 19:07:47 +0200

    game view

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ### new Challenge 
 ### Ok i watched 3 videos a bout the game and how play it 
 # rock beats Scissors , Scissors beats paper , paper beats rock
-### I will use my library to build UI ,  i built it by my self and i will be happy to discuss any part of it
+### I will use my library to build UI ,  i built it by my self and i will be happy to discuss any part of it it in mwn.js
 ### I generate 3 png img using illustraor, i because i didn't found that in my icon library 
 ### In fact, my library is more complicated than what I write of code here , so I will try to use it, but in a simple way to explain my point, I will make a tools for creating HTML pieces, to be clear and I will Call it :
 
--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const C_H = (typ, cls, inner, eId) => {
     /*  U_CSS generate css classes and append
      page style if class is not exists */
     e.className = U_CSS(cls);
-    if (typ == "img") { e.src = inner; } else { e.innerHTML = inner; }
+    if (typ == "img") { e.src = inner; } else { if (inner !== "") { e.innerHTML = inner; } }
     if (eId) {
         e.id = eId;
     }
@@ -28,6 +28,8 @@ const A_H = (p, c) => {
 const gameView = () => {
     var score = 0;
     var sysScoreN = 0;
+
+    // game score
     var userScore = C_H("div", "POS_FX PD_3 LL_0 TT_0 _MR_20 B_GRE4 F_B B_R_15", "", "userScore");
     var userI = C_H("icon", "ICO-user-alt-7 PD_5 F_S_30 F_W MWIco", "", false);
     userScore.appendChild(userI);
@@ -39,10 +41,22 @@ const gameView = () => {
     var body = E_T("body")[0];
     body.appendChild(userScore);
     body.appendChild(sysScore);
+    // btArea
+    var btArea = E_I("btArea");
+    var startBt = C_H("icon", "ICO-play-alt-3  PD_50 cr B_YE3 F_S_50 F_W MWIco", "", false);
+    startBt.addEventListener("click", () => {
+        gameStart();
+    });
+    var ti2 = C_H("h2", "F_B", "PLAY", false);
+
+
+    A_H(btArea, [startBt, ti2]);
+
+
 }
 const _ = (function() {
 
-    //Hi i use a small part of my library for css 
+    //Hi i use a small part of my library for css at mwn.js
     startStyle();
     S_SW_TH();
     //i will happy to discuss any part of it
@@ -53,14 +67,16 @@ const _ = (function() {
     2-play screen
     3-buttons 
     */
+    // basic contanier
     var root = E_I("root");
     var logoArea = C_H("div", "WW H_P_30 NH_200 MD_T", "", "logoArea");
-    var playScreen = C_H("div", "WW H_P_30 NH_200 ", "", "playScreen");
-    var btArea = C_H("div", "WW H_P_30 NH_200 ", "", "btArea");
-    var logo = C_H("img", "MD W_150", "/img/icon.png", "logo");
+    var playScreen = C_H("div", "WW H_P_30 NH_200 MD_T", "", "playScreen");
+    var btArea = C_H("div", "WW H_P_30 NH_200 MD_T", "", "btArea");
+    var logo = C_H("img", "MD W_150 mT_30", "/img/icon.png", "logo");
     var ti = C_H("h2", "F_B MD_T WW F_S_30", "RPS", "ti1");
-    var p = C_H("p", "F_B F_S_50", "Rock  -  Paper  -  Scissors <br>", "p1");
-    A_H(logoArea, [logo, ti, p]);
+    var p = C_H("p", "F_B F_S_30", "Rock  -  Paper  -  Scissors <br>", false);
+    A_H(logoArea, [logo]);
+    A_H(playScreen, [ti, p]);
     A_H(root, [logoArea, playScreen, btArea]);
     gameView();
 })();
