a0f796e46254348af12a9e276575f68381db5fe1
Mohamed El Azazy <moregroupx@yahoo.com>
2021-06-21 17:59:32 +0200

    C_H i like it that will make small code

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 # rock beats Scissors , Scissors beats paper , paper beats rock
 ### I will use my library to build UI ,  i built it by my self and i will be happy to discuss any part of it
 ### I generate 3 png img using illustraor, i because i didn't found that in my icon library 
+### In fact, my library is more complicated than what I write of code here , so I will try to use it, but in a simple way to explain my point, I will make a tool for creating HTML pieces, to be clear and I will Call it C_H = Create Html .
--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 //main
+const C_H = (typ, cls, inner, eId) => {
+    var e = CE_(typ);
+    e.className = U_CSS(cls);
+    e.innerHTML = inner;
+    e.id = eId;
+    return e;
+}
 const _ = (function() {
 
     //Hi i use a small part of my library for css 
@@ -13,15 +20,9 @@ const _ = (function() {
     3-buttons 
     */
     var root = E_I("root");
-    var logoArea = CE_("div");
-    logoArea.className = U_CSS("WW H_P_30 NH_200 B_YE1");
-    var playScreen = CE_("div");
-    playScreen.className = U_CSS("WW H_P_30 NH_200 B_YE5");
-    var btArea = CE_("div");
-    btArea.className = U_CSS("WW H_P_30 NH_200 B_YE8");
-    logoArea.innerHTML = "test";
-    playScreen.innerHTML = "test";
-    btArea.innerHTML = "test";
+    var logoArea = C_H("div", "WW H_P_30 NH_200 B_YE1", "test", "logoArea");
+    var playScreen = C_H("div", "WW H_P_30 NH_200 B_YE1", "test", "playScreen");
+    var btArea = C_H("div", "WW H_P_30 NH_200 B_YE1", "test", "btArea");
     root.appendChild(logoArea);
     root.appendChild(playScreen);
     root.appendChild(btArea);
