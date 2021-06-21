c8a3137ded695b094c2227f2c00aa7b843b13679
Mohamed El Azazy <moregroupx@yahoo.com>
2021-06-21 17:29:43 +0200

    create Body

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 ### new Challenge 
 ### Ok i watched 3 videos a bout the game and how play it 
 # rock beats Scissors , Scissors beats paper , paper beats rock
-### I will use my library to build UI ,  i built it by my self and i will happy to discuss any part of it
+### I will use my library to build UI ,  i built it by my self and i will be happy to discuss any part of it
 ### I generate 3 png img using illustraor, i becase i didn't found that in my icon library 
--- js/main.js
+++ js/main.js
@@ -5,5 +5,24 @@ const _ = (function() {
     startStyle();
     S_SW_TH();
     //i will happy to discuss any part of it
-
+    /*
+    as defult my contaniar called root
+    i will split to three parts
+    1-logoArea
+    2-play screen
+    3-buttons 
+    */
+    var root = E_I("root");
+    var logoArea = CE_("div");
+    logoArea.className = U_CSS("WW H_P_30 B_YE1");
+    var playScreen = CE_("div");
+    playScreen.className = U_CSS("WW H_P_30 B_YE5");
+    var btArea = CE_("div");
+    btArea.className = U_CSS("WW H_P_30 B_YE8");
+    logoArea.innerHTML = "test";
+    playScreen.innerHTML = "test";
+    btArea.innerHTML = "test";
+    root.appendChild(logoArea);
+    root.appendChild(playScreen);
+    root.appendChild(btArea);
 })();
